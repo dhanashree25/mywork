dcdbb42f17e8ba6f1a2412c83bbbdcca0baee472
Dhanu <dhanashree.deval@neulion.com>
2018-05-23 16:07:03 +0000

    initial commit

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # dce_spark_jobs
 
 This is the repo for spark jobs to read all the DICE data
+
+Step1: Upload the jobname.py to S3 bucket
+Step2: Upload helper JAR files and Classfiles to bucket
+Step3: Upload config file to bucket
+Step4: Ensure that EMR cluster is running
+Step5: Execute spark job in cluster by adding it to cluster using:
+    aws emr add - steps - -region region - -cluster - id clusterid - -steps Type = Spark, Name = "appname", ActionOnFailure = CONTINUE, Args = [--packages, s3: // helperclassfile, --jars, s3: // helper.jars, s3: // bucket / jobname.py]
+Step6: Check job execution logs on console / sshing into cluster
---
+++ dice_events_read.py
@@ -0,0 +1,115 @@
+# ============= =======================================================================================================
+# Spark Job to read event messages from dice bucket and insert into database
+# Version-1.0
+# =====================================================================================================================
+import datetime
+import json
+from io import BytesIO
+
+from pyspark.streaming import StreamingContext
+from pyspark.sql import Row, SparkSession
+import pyspark.sql.functions as func
+from pyspark.sql import SQLContext
+from pyspark.sql.types import *
+
+
+def getSparkSessionInstance(sparkConf):
+    if ('sparkSessionSingletonInstance' not in globals()):
+        globals()['sparkSessionSingletonInstance'] = SparkSession\
+            .builder\
+            .config(conf=sparkConf)\
+            .getOrCreate()
+    return globals()['sparkSessionSingletonInstance']
+
+
+def json_decoder_generator(contents):
+    pos = 0
+    while True:
+        trial = contents.find('}', pos)
+        if trial == -1:
+            return
+        trial_json = contents[: trial + 1]
+        try:
+            out = json.loads(trial_json)
+        except ValueError:
+            pos = trial + 1
+        else:
+            contents = contents[trial + 1:]
+            pos = 0
+            yield {
+		 "action": out.get("action"),
+      		 "client_ip": out.get("clientIp"),
+                 "action": out["payload"]["action"],
+                 "session_id": out["payload"].get("data", {}).get("cid"),
+                 "device": out["payload"].get("data", {}).get("device"),
+                 "video_id": out["payload"].get("video"),
+                 "realm": out["realm"],
+                 "customer_id": out["customerId"],
+                 "country": out["country"],
+                 "town": out["town"],
+                 "progress": out["payload"].get("progress"),
+                 "ts": datetime.datetime.strptime(out["ts"], "%Y-%m-%d %H:%M:%S"),
+                 "started_at": datetime.datetime.utcfromtimestamp(out["payload"].get("data", {}).get("startedAt", 0) / 1000),
+             }
+
+
+if __name__ == "__main__":
+    	import time
+    	start_time = time.time()
+    	# Initialize a SparkContext with a name
+    	sc = SparkSession.builder.appName("ReadDiceData").getOrCreate()
+    	# Initialize SQL context
+	sqlc = SQLContext(sc)
+    	# get_config
+    	config_file = sc.sparkContext.textFile('s3n://test-dce-spark/config.txt')
+    	config = config_file.map(lambda x: (
+        	 x.split(",")[0],
+            	x.split(",")[1])).collectAsMap()
+
+    	# database conn details
+    	postgres_url = config["postgres_url"]
+    	postgres_properties = {
+        	"user": config["postgres_user"],
+        	"password": config["postgres_password"],
+        	"driver": config["postgres_driver"]
+   		 }
+    	mode = 'append'
+
+    	# bucket details
+    	mount_name = 'dce-tracking'
+    	sc._jsc.hadoopConfiguration().set(
+        	"fs.s3n.awsAccessKeyId",
+        	config["access_key_id"])
+    	sc._jsc.hadoopConfiguration().set(
+        	"fs.s3n.awsSecretAccessKey",
+        	config["access_secret_key"])
+
+	# Read S3 bucket files
+    	readRdd = sc.sparkContext.textFile("testdata/test.json")
+    	# readRdd = sc.sparkContext.textFile("s3n://dce-tracking/prod/2018/05/01/*")
+
+    	# Decode Json files
+	jsonRdd=readRdd.flatMap(json_decoder_generator)  # repartition(100)
+    	# Specify the dataframe schema.. can be added to config
+	schema=StructType([StructField("customer_id", StringType(), True),
+                         StructField("video_id", IntegerType(), True),
+                         StructField("started_at", TimestampType()),
+                         StructField("realm", StringType(), True),
+                         StructField("town", StringType(), True),
+                         StructField("country", StringType(), True),
+                         StructField("session_id", StringType(), True),
+                         StructField("client_ip", StringType()),
+			 StructField("action", IntegerType(), True),
+                         StructField("device", StringType()),
+                         StructField("progress", IntegerType(), True),
+                         StructField("ts", TimestampType(), True),
+      			 StructField("ta", StringType()),
+                         ])
+    	rowDataFrame=sqlc.createDataFrame(data=jsonRdd, schema=schema)
+	# Write to database
+    	rowDataFrame.write.jdbc(
+        	url=postgres_url,
+        	table=config["tablename_events"],
+        	mode=mode,
+        	properties=postgres_properties)
+    	print ("time- %s " % (time.time() - start_time))
