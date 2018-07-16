d4684c64bc354f09214600fe632c5420f03c16f0
dhanashree25 <dhanu25@gmail.com>
2018-07-16 15:55:53 +0100

    claning up the files

--- scala/emr-load-dce-jobs.sh
+++ scala/emr-load-dce-jobs.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
 set -u -x
-BUCKET=${STACK_NAME}-${PLATFORM}-dce-test-emr
 CLUSTER=${STACK_NAME}-${PLATFORM}-spark-emr
 CLUSTER_ID=$(aws emr list-clusters --region=eu-west-1 --active | jq  -r '.[][] | select(.Name == "'${CLUSTER}'") | .Id')
 
@@ -14,6 +13,6 @@ var_date=$(psql -t -c "select to_char(max(start_at), 'YYYY/mm/dd')  from live_pl
 #var_date=$(date -d '-1 day' '+%Y/%d/%m')
 echo $var_date
 
-sed -i "s@var_date@$var_date@g" step_vod.json
+sed -i "s@var_date@$var_date@g" step_job.json
 
-aws emr add-steps --region ${region} --cluster-id ${CLUSTER_ID} --steps file://step_vod.json
+aws emr add-steps --region eu-west-1 --cluster-id ${CLUSTER_ID} --steps file://step_job.json
