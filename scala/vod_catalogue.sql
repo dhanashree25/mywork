f7a56be26131566dfd975f458bd0a24ef535074e
Iszak Bryan <iszak.bryan@neulion.com>
2018-07-20 16:08:26 +0100

    Added schema for live and VOD catalogues
    This is a back port from the live schema, so there is a slight change
    in particular description is larger as the CSV import exceeds it.

--- scala/vod_catalogue.sql
+++ scala/vod_catalogue.sql
@@ -1,14 +1,14 @@
-CREATE TABLE "public"."catalogue2" (
+CREATE TABLE "public"."vod_catalogue" (
     "video_id" INT NOT NULL,
     "video_dve_id" INT NOT NULL,
     "realm_id" INT NOT NULL,
     "title" VARCHAR(1024) NOT NULL,
-    "description" VARCHAR(1024) NOT NULL,
+    "description" VARCHAR(4096) NOT NULL,
     "duration" INT NOT NULL,
     "thumbnail_url" VARCHAR(1024) NOT NULL,
     "deleted" BOOLEAN NOT NULL,
     "draft" BOOLEAN NOT NULL,
     "tags" VARCHAR(1024),
     "imported_at" TIMESTAMP,
-    "updated_at" TIMESTAMP NOT NULL
+    "updated_at" TIMESTAMP
 ) COMPOUND SORTKEY(realm_id, updated_at, deleted, draft);
