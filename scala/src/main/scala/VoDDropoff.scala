d0c81cae001e97339257621d1e607c8a387b1570
dhanashree25 <dhanu25@gmail.com>
2018-07-20 14:24:47 +0100

    corrected queries for vod_catalogue

--- scala/src/main/scala/VoDDropoff.scala
+++ scala/src/main/scala/VoDDropoff.scala
@@ -32,35 +32,37 @@ object VoDDropoff extends Main {
     val df = spark.read.redshift(spark)
       .option("dbtable", "(select session_id, customer_id, realm_id, video_id, min(start_at) start_time, max(end_at) end_time, max(duration) progress from vod_play group by session_id, customer_id, realm_id, video_id)") 
       .load()
-    
+
     val vod_catalogue = spark
       .read
       .redshift(spark)
-      .option("dbtable", "vod_catalogue")
+      .option("dbtable", "(select video_dve_id,max(duration) duration from vod_catalogue group by video_dve_id)")
       .load()
-//                          Table "public.vod_dropoff"
-//   Column    |          Type          | Collation | Nullable | Default 
-//-------------+------------------------+-----------+----------+---------
-// realm_id    | integer                |           |          | 
-// video_id    | integer                |           |          | 
-// customer_id | character varying(256) |           |          | 
-// session_id  | character varying(256) |           |          | 
-// year        | integer                |           |          | 
-// dow         | integer                |           |          | 
-// month       | integer                |           |          | 
-// day         | integer                |           |          | 
-// hour        | integer                |           |          | 
-// progress    | integer                |           |          | 
-// duration    | integer                |           |          | 
-// perc_drop   | double precision       |           |          | 
-//COMPOUND SORTKEY(realm_id, video_id, customer_id);
+
+//                              Table "public.vod_dropoff"
+//         Column     |            Type             | Collation | Nullable | Default 
+//    ----------------+-----------------------------+-----------+----------+---------
+//     realm_id       | integer                     |           |          | 
+//     video_id       | integer                     |           |          | 
+//     customer_id    | character varying(32)       |           | not null | 
+//     year           | integer                     |           |          | 
+//     month          | integer                     |           |          | 
+//     day            | integer                     |           |          | 
+//     dow            | integer                     |           |          | 
+//     hour           | integer                     |           |          | 
+//     start_at       | timestamp without time zone |           |          | 
+//     video_duration | integer                     |           | not null | 
+//     progress       | integer                     |           | not null | 
+//     perc_drop      | double precision            |           |          | 
+
+//     COMPOUND SORTKEY(realm_id, video_id, customer_id);
     val updates = df.
         join(vod_catalogue, df.col("video_id") === vod_catalogue.col("video_dve_id")) 
         .select(
             df.col("realm_id"),
             df.col("video_id"),
             df.col("customer_id"),
-            df.col("session_id"),
+            df.col("start_time").alias("start_at"),
             year(col("start_time")).alias("year"),
             weekofyear(col("start_time")).alias("dow"),
             month(col("start_time")).alias("month"),
@@ -69,14 +71,17 @@ object VoDDropoff extends Main {
             col("progress"),
             col("duration"),
             (col("progress")*100/col("duration") ).alias("perc_drop"))
-            
+        .where(col("duration")=!= 0)
+       
         if (!cli.dryRun) {
               print("Writing to table") 
+              
           updates.write
             .redshift(spark)
             .option("dbtable", "vod_dropoff")
             .mode(SaveMode.Overwrite)
             .save()
+            print("done")
         } else {
           updates.show()
     }
--- scala/vod_dropoff.sql
+++ scala/vod_dropoff.sql
@@ -1,19 +1,4 @@
-                    Table "public.video_dropouts"
-   Column    |            Type             | Collation | Nullable | Default 
--------------+-----------------------------+-----------+----------+---------
- video_id    | integer                     |           |          | 
- customer_id | character varying(25)       |           |          | 
- start_time  | timestamp without time zone |           |          | 
- year        | double precision            |           |          | 
- month       | double precision            |           |          | 
- day         | double precision            |           |          | 
- dow         | double precision            |           |          | 
- hour        | double precision            |           |          | 
- progress    | integer                     |           |          | 
- duration    | integer                     |           |          | 
- perc_drop   | integer                     |           |          | 
- 
-create table vod_dropoff(
+ create table vod_dropoff(
 	realm_id integer,
 	video_id integer,
 	customer_id varchar(32) not null,
@@ -23,7 +8,7 @@ create table vod_dropoff(
     day int,
     dow int,
     hour int,
-	started_at timestamp,
+	start_at timestamp,
 	video_duration int not null,
 	progress int not null,
 	perc_drop float,
