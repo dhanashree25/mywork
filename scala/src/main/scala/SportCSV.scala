00eb2f024ef7b14060cf8a00aece92174a89bd5b
Iszak Bryan <iszak.bryan@neulion.com>
2018-07-24 10:46:13 +0100

    Added sports batch job to process CSV

---
+++ scala/src/main/scala/SportCSV.scala
@@ -0,0 +1,45 @@
+import com.diceplatform.brain.implicits._
+import org.apache.spark.sql._
+import org.apache.spark.sql.types._
+
+object SportCSV extends Main {
+  val schema = StructType(
+    Array(
+      StructField("sport_id", StringType, nullable=false),
+      StructField("name", StringType, nullable=false)
+    )
+  )
+
+
+  def main(args: Array[String]): Unit = {
+    val parser = csvParser
+
+    parser.head("Extract-Transform-Load (ETL) task for sports stored in CSV")
+
+    var cli: CSVConfig = CSVConfig()
+    parser.parse(args, cli) match {
+      case Some(c) => cli = c
+      case None => System.exit(1)
+    }
+
+    val realms = spark.read
+      .option("header", value=cli.header)
+      .option("sep", value=cli.separator)
+      .option("escape", value="\"")
+      .schema(schema)
+      .csv(cli.path)
+
+    print("-----total------", realms.count())
+
+    if (!cli.dryRun) {
+      realms
+        .write
+        .redshift(spark)
+        .option("dbtable", "sport")
+        .mode(SaveMode.Overwrite)
+        .save()
+    } else {
+      realms.show()
+    }
+  }
+}
