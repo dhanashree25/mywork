c5946847948418e1f9854d1775c018f7335862a0
Iszak Bryan <iszak.bryan@neulion.com>
2018-07-24 10:32:54 +0100

    Removed fixtures and instead create a batch job to enforce schema

--- scala/realm.sql
+++ scala/realm.sql
@@ -2,17 +2,3 @@ CREATE TABLE "public"."realm" (
     "realm_id" INT NOT NULL PRIMARY KEY,
     "name" VARCHAR(256) NOT NULL UNIQUE
 ) COMPOUND SORTKEY("realm_id", "name");
-
-INSERT INTO "realm" ("realm_id", "name") VALUES
-    (2, 'dce.ufc'),
-    (5, 'Unknown'),
-    (6, 'Unknown'),
-    (7, 'dce.epl'),
-    (9, 'dce.pbr'),
-    (10, 'dce.sport'),
-    (11, 'dce.ssport'),
-    (12, 'Unknown'),
-    (13, 'Unknown'),
-    (14, 'Unknown'),
-    (15, 'dce.fivb'),
-    (16, 'dce.seriea');
