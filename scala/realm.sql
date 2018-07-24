df102d4842c28bd35088569945f533880cf5c014
Iszak Bryan <iszak.bryan@neulion.com>
2018-07-24 11:18:56 +0100

    Make realm_id and sport_id unique, not implied with primary key
    Due to how Redshift works, when we try and create a foreign key
    constraint it errors about the column not being unique.

--- scala/realm.sql
+++ scala/realm.sql
@@ -1,4 +1,4 @@
 CREATE TABLE "public"."realm" (
-    "realm_id" INT NOT NULL PRIMARY KEY,
+    "realm_id" INT NOT NULL UNIQUE PRIMARY KEY,
     "name" VARCHAR(256) NOT NULL UNIQUE
 ) COMPOUND SORTKEY("realm_id", "name");
--- scala/sport.sql
+++ scala/sport.sql
@@ -1,5 +1,5 @@
 CREATE TABLE "public"."sport" (
-    "sport_id" INT NOT NULL PRIMARY KEY,
+    "sport_id" INT NOT NULL UNIQUE PRIMARY KEY,
     "name" VARCHAR(256) NOT NULL UNIQUE
 ) COMPOUND SORTKEY("sport_id", "name");
 
