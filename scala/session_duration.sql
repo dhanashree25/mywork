955dff619671a7c2767ccc3ebab9dca455e54b94
Iszak Bryan <iszak.bryan@neulion.com>
2018-07-27 11:43:43 +0100

    Use consistent indentation

--- scala/session_duration.sql
+++ scala/session_duration.sql
@@ -1,14 +1,14 @@
 create table session_duration(
-	realm_id int not null,
-	session_id varchar(64)  not null,
-	customer_id varchar(32) not null,
-	duration int defualt 0,
-	started_at timestamp,
-	start_at timestamp,
-	end_at timestamp,
-	country char(2),
-	town varchar(256),
-	foreign key(realm_id) references realm(realm_id),
-	foreign key(country) references country(alpha_2)
-	)
-	COMPOUND SORTKEY(realm_id, session_id, customer_id);
+    realm_id int not null,
+    session_id varchar(64)  not null,
+    customer_id varchar(32) not null,
+    duration int defualt 0,
+    started_at timestamp,
+    start_at timestamp,
+    end_at timestamp,
+    country char(2),
+    town varchar(256),
+    foreign key(realm_id) references realm(realm_id),
+    foreign key(country) references country(alpha_2)
+)
+COMPOUND SORTKEY(realm_id, session_id, customer_id);
