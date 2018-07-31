0639fbcc7d1f609dc878fbf214909da292b1ab9f
Iszak Bryan <iszak.bryan@neulion.com>
2018-07-31 15:58:27 +0100

    Added SQL file which describes the permissions granted to each users

---
+++ scala/permissions.sql
@@ -0,0 +1,8 @@
+--- John Stones
+GRANT SELECT ON TABLE "public"."revenue_hourly" TO "john.stones";
+GRANT SELECT ON TABLE "public"."revenue_daily" TO "john.stones";
+GRANT SELECT ON TABLE "public"."revenue_weekly" TO "john.stones";
+GRANT SELECT ON TABLE "public"."revenue_hourly" TO "john.stones";
+
+--- Onochie Anyanwu
+GRANT SELECT ON ALL TABLES IN SCHEMA "public" TO "onochie.anyanwu";
