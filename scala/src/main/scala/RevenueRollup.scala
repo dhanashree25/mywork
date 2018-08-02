43284c9eba5237e0022b1a136016bb35943afee0
Iszak Bryan <iszak.bryan@neulion.com>
2018-08-02 16:38:37 +0100

    Test revenue roll up

--- scala/src/main/scala/RevenueRollup.scala
+++ scala/src/main/scala/RevenueRollup.scala
@@ -1,7 +1,7 @@
 import com.diceplatform.brain.implicits._
 import org.apache.spark.sql._
 import org.apache.spark.sql.functions._
-import org.apache.spark.sql.types.{DateType, IntegerType}
+import org.apache.spark.sql.types.{DateType, DecimalType, IntegerType}
 
 case class RollupConfig(path: String = "", dryRun: Boolean = false, from: String = "", to: String = "", window: String = "hour")
 object RevenueRollup extends Main{
@@ -32,6 +32,21 @@ object RevenueRollup extends Main{
       .text("the date range to select to (exclusive). Formatted at yyyy-MM-dd HH:mm:ss")
   }
 
+  def selectPayments(df: DataFrame, from: String = "", to: String = ""): DataFrame = {
+    var payments = df
+
+    if (from != "") {
+      payments = payments.where(col("ts") >= from)
+
+      if (to != "") {
+        payments = payments.where(col("ts") < to)
+      }
+    }
+
+    payments
+  }
+
+
   def main(args: Array[String]): Unit = {
     var cli: RollupConfig = RollupConfig()
     parser.parse(args, cli) match {
@@ -46,13 +61,11 @@ object RevenueRollup extends Main{
       case period => period + "ly"
     })
 
-    val exchange_rates = spark
+    val exchangeRates = spark
       .read
       .redshift(spark)
       .option("dbtable", "exchange_rate")
       .load()
-
-    val exchange_rate_columns = exchange_rates.dtypes.map(dtype => dtype._1)
 
     // Get all payments within range
     var payments = spark
@@ -61,100 +74,22 @@ object RevenueRollup extends Main{
       .option("dbtable", "payment")
       .load()
 
-    if (cli.from != "") {
-      payments = payments.where(col("ts") >= cli.from)
+    payments = selectPayments(payments, cli.from, cli.to)
 
-      if (cli.to != "") {
-        payments = payments.where(col("ts") < cli.to)
-      }
-    }
+    val revenueRollup = rollupRevenue(payments, cli.window)
 
+    val revenueRollupInUSD = withColumnInCurrency(revenueRollup, exchangeRates, "USD")
 
-
-    // Calculate a cube of all the dimensions we're interested in (realm_id, payment_provider, currency)
-    // Aggregate by the amount
-    val revenue_rollup = payments
-      .na.fill(0, Seq("amount_with_tax"))
-      .where(col("amount_with_tax") =!= 0.0)
-      .cube(
-        col("realm_id"),
-        col("payment_provider"),
-        col("currency"),
-        window(col("ts"), "1 " + cli.window).getItem("start").alias("ts")
-      )
-      .agg(
-        sum(col("amount_with_tax")).alias("amount")
-      )
-      .na.drop() // Drop cube by partial dimensions
-      .as("r")
-      .withColumnRenamed("currency", "source_currency")
-
-    val revenue_rollup_columns = revenue_rollup.dtypes.map(dtype => col(dtype._1))
-
-    // Add ts_date/ts_weekday to optimize joins, in theory Spark should only calculate it once.
-    val revenue_with_dates = revenue_rollup
-        .withColumn("ts_date", col("ts").cast(DateType))
-        .withColumn("ts_weekday", date_format(col("ts_date"), "E"))
-
-    // Join rollup with EUR exchange rates, as we only have A <-> EUR
-    val revenue_with_eur = revenue_with_dates
-      .where(revenue_with_dates.col("source_currency") =!= "USD") // Exclude USD as already in the correct currency
-      .join(
-        right=exchange_rates,
-        joinExprs=exchange_rates.col("currency") === revenue_with_dates.col("source_currency") and whenWeekday(revenue_with_dates, exchange_rates),
-        joinType="left_outer"
-      )
-      .withColumn("amount_eur", (revenue_with_dates.col("amount") / col("rate")).cast(IntegerType))
-      .drop(exchange_rate_columns: _*)
-
-    // Check for nulls as we do an left outer join, this is to prevent dropping records
-    val null_with_eur = revenue_with_eur.where(col("amount_eur").isNull).count()
-    if (null_with_eur > 0) {
-      throw new Exception("Rows exist with null EUR amount, this would be because the exchange rate doesn't exist")
-    }
-
-    // Join with USD exchange rates, so we can get EUR -> USD
-    val revenue_with_usd = revenue_with_eur
-      .join(
-        right=exchange_rates,
-        joinExprs=exchange_rates.col("currency") === "USD" and whenWeekday(revenue_with_eur, exchange_rates),
-        joinType="left_outer"
-      )
-      .withColumn("amount_usd", (revenue_with_eur.col("amount_eur") * col("rate")).cast(IntegerType))
-      .drop(exchange_rate_columns: _*)
-
-    // Check for nulls as we do an left outer join, this is to prevent dropping records
-    val null_with_usd = revenue_with_usd.where(col("amount_usd").isNull).count()
-    if (null_with_usd > 0) {
-      throw new Exception("Rows exist with null USD amount, this would be because the exchange rate doesn't exist")
-    }
-
-    // Only select the original columns + amount_usd to save
-    val eur_revenue = revenue_with_usd
-      .select(
-        revenue_rollup_columns ++ Array[Column](revenue_with_usd.col("amount_usd")): _*
-      )
-
-    val usd_revenue = revenue_rollup
-      .where(revenue_with_dates.col("source_currency") === "USD")
-      .withColumn("amount_usd", col("amount"))
-
-    val date_trunc_format = cli.window match {
+    val dateTruncateFormat = cli.window match {
         case "month" => "MM"
         case "week" => "WEEK"
         case "day" => "DD"
         case "hour" => "HOUR"
     }
 
-    val revenue = eur_revenue.union(usd_revenue)
-      .select(
-        col("realm_id"),
-        col("payment_provider"),
-        col("source_currency").as("currency"),
-        col("amount"),
-        col("amount_usd"),
-        date_trunc(date_trunc_format, col("ts")).alias("ts")
-      )
+    val revenue = revenueRollupInUSD
+      .withColumnRenamed("amount_with_tax_usd", "amount_usd")
+      .withColumn("ts", date_trunc(dateTruncateFormat, col("ts")))
 
     if (!cli.dryRun) {
       revenue
@@ -169,23 +104,98 @@ object RevenueRollup extends Main{
  }
 
   /**
+    * Adds a new column (amount_<currency>) which is in the requested currency
+    *
+    * It expects the data frame to have the column "amount_with_tax", "currency", and "ts"
+    * It expects the exchange rate data frame to have the columns "currency", "rate" and "date"
+    */
+  def withColumnInCurrency(df: DataFrame,
+                           exchangeRatesDf: DataFrame,
+                           column:String = "amount_with_tax",
+                           currency:String = "USD"
+                  ): DataFrame = {
+    val exchangeRateColumns = exchangeRatesDf.dtypes.map(dtype => dtype._1)
+
+    // alias otherwise when dropping it gets confused
+    val dfExtra = df
+      .withColumn("ts_date", col("ts").cast(DateType))
+      .withColumn("ts_weekday", date_format(col("ts_date"), "E"))
+      .withColumnRenamed("currency", "source_currency")
+
+    val amountInEUR = dfExtra
+      .where(dfExtra.col("source_currency") =!= currency) // Exclude currency as already in the correct currency
+      .join(
+        right = exchangeRatesDf,
+        joinExprs = exchangeRatesDf.col("currency") === dfExtra.col("source_currency") and closestDay(dfExtra, exchangeRatesDf),
+        joinType = "left_outer"
+      )
+      .withColumn(s"${column}_eur", dfExtra.col(column) / exchangeRatesDf.col("rate"))
+      .drop(exchangeRateColumns: _*)
+
+
+    // Check for nulls as we do an left outer join, this is to prevent dropping records
+    val nullEURCount = amountInEUR.where(col(s"${column}_eur").isNull).count()
+    if (nullEURCount > 0) {
+      throw new Exception("Rows exist with null EUR amount, this would be because the exchange rate doesn't exist")
+    }
+
+    // Join with USD exchange rates, so we can get EUR -> USD
+    val dfInCurrency = amountInEUR
+      .join(
+        right=exchangeRatesDf,
+        joinExprs=exchangeRatesDf.col("currency") === currency.toUpperCase() and closestDay(amountInEUR, exchangeRatesDf),
+        joinType="left_outer"
+      )
+      .withColumn(s"${column}_" + currency.toLowerCase(), round(amountInEUR.col(s"${column}_eur") * exchangeRatesDf.col("rate")).cast(IntegerType))
+      .drop(exchangeRateColumns: _*)
+      .drop(s"${column}_eur")
+
+    // Check for nulls as we do an left outer join, this is to prevent dropping records
+    val nullCurrencyCount = dfInCurrency.where(col(s"${column}_usd").isNull).count()
+    if (nullCurrencyCount > 0) {
+      throw new Exception(s"Rows exist with null ${currency.toUpperCase} amount, this would be because the exchange rate doesn't exist")
+    }
+
+    // Add back rows already in the currency
+    val dfAlreadyInCurrency = df
+      .where(df.col("currency") === currency)
+      .withColumn(s"${column}_usd", col(column))
+
+    dfInCurrency
+      .drop("ts_date")
+      .drop("ts_weekday")
+      .withColumnRenamed("source_currency", "currency")
+      .union(dfAlreadyInCurrency)
+  }
+
+  /**
+    * Calculate the aggregate of the revenue of a window duration by realm, payment provider and currency
+    */
+  def rollupRevenue(df: DataFrame, windowDuration: String): DataFrame = {
+    df
+      .na.fill(0, Seq("amount_with_tax"))
+      .where(col("amount_with_tax") =!= 0.0)
+      .cube(
+        col("realm_id"),
+        col("payment_provider"),
+        col("currency"),
+        window(col("ts"), "1 " + windowDuration).getItem("start").alias("ts")
+      )
+      .agg(
+        sum(col("amount_with_tax")).alias("amount")
+      )
+      .na.drop() // Drop cube by partial dimensions
+  }
+
+
+  /**
     * A condition which selects the closest weekday
     *
     * If the day is Sunday, it will select Friday
     * If the day is Saturday, it will select Friday
     * Otherwise, select the current day
     */
-  private def whenWeekday(df1: DataFrame, df2: DataFrame): Column = {
-    if (!df1.dtypes.exists(dtype => dtype._1 == "ts_date")) {
-      throw new Exception("Data frame 1 does not have the column ts_date")
-    }
-    if (!df1.dtypes.exists(dtype => dtype._1 == "ts_weekday")) {
-      throw new Exception("Data frame 1 does not have the column ts_weekday")
-    }
-    if (!df2.dtypes.exists(dtype => dtype._1 == "date")) {
-      throw new Exception("Data frame 2 does not have the column date")
-    }
-
+  private def closestDay(df1: DataFrame, df2: DataFrame): Column = {
     when(
       df1.col("ts_weekday") === "Sun",
       date_sub(df1.col("ts_date"), 2) === df2.col("date")
