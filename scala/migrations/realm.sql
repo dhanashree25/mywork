a1d4a2c7e46bac184c360f6b5eca0c7041f94a19
Iszak Bryan <iszak.bryan@neulion.com>
2018-08-30 13:49:43 +0100

    Added fixtures for realm, added missing missing ones.

--- scala/migrations/realm.sql
+++ scala/migrations/realm.sql
@@ -1,4 +1,29 @@
+BEGIN TRANSACTION READ WRITE;
+
+DROP TABLE IF EXISTS "public"."realm";
+
 CREATE TABLE "public"."realm" (
     "realm_id" INT NOT NULL UNIQUE PRIMARY KEY,
     "name" VARCHAR(256) NOT NULL UNIQUE
 ) COMPOUND SORTKEY("realm_id", "name");
+
+INSERT INTO "public"."realm" ("realm_id", "name") VALUES
+  (1, 'dce.ufc'),
+  (2, 'test-dve'),
+  (3, 'prod-dve'),
+  (4, 'dev-dve'),
+  (5, 'sampaoli'),
+  (7, 'dce.ent'),
+  (8, 'dce.sport'),
+  (9, 'dce.epl'),
+  (10, 'dce.pbr'),
+  (11, 'dce.ash'),
+  (12, 'dce.ssport'),
+  (13, 'dce.jo'),
+  (14, 'dce.sandbox'),
+  (15, 'devops'),
+  (16, 'dce.wwe'),
+  (17, 'dce.fivb'),
+  (18, 'dce.seriea'),
+  (19, 'dce.imgott');
+COMMIT;
